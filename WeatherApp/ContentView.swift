f1d20bfe42da93c2a85f20738e5a68f9162b1244
Robert Kotrutsa <tr3asur3island@gmail.com>
2026-04-27 16:15:31 +0200

    feat: implement search bar and main weather UI

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -8,14 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var city: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView {
+            VStack(spacing: 20) {
+                HStack {
+                    TextField("\(Image(systemName: "magnifyingglass")) Search city...", text: $city)
+                        .padding(7)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(Color.gray, lineWidth: 1)
+                        )
+                    
+                    Button {
+                        print("Search tapped")
+                    } label: {
+                        Text("Search")
+                    }
+                    .padding(7)
+                    .foregroundStyle(.gray)
+                    
+                }
+                VStack(alignment: .leading) {
+                    Text("London")
+                    HStack {
+                        Text("13°")
+                        Image(systemName: "sun.max.fill")
+                    }
+                    Text("Overcast clouds")
+                    HStack{
+                        Text("Max: 17°")
+                        Text("Min: 7°")
+                    }
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .foregroundStyle(.gray)
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(Color.gray)
+                )
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
